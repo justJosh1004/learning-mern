4e1bc8e1c4d2ac783c0136742d399cbcf39f3014
justJosh1004 <j.joson@live.com>
2019-03-06 11:47:58 -0500

    completed users and profile. started model for posts.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb://josh:josh@cluster0-shard-00-00-5yeui.mongodb.net:27017,cluster0-shard-00-01-5yeui.mongodb.net:27017,cluster0-shard-00-02-5yeui.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+    'mongodb://josh:josh@cluster0-shard-00-00-5yeui.mongodb.net:27017,cluster0-shard-00-01-5yeui.mongodb.net:27017,cluster0-shard-00-02-5yeui.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  secretOrKey: 'secret'
 };
