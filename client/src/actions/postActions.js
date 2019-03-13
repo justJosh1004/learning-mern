f66212fc57596fca7bc43c6c4cc45d22c4e3517d
justJosh1004 <j.joson@live.com>
2019-03-13 14:23:09 -0400

    display posts

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { ADD_POST, GET_ERRORS } from './types';
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from './types';
 
 // Add Post
 export const addPost = postData => dispatch => {
@@ -15,4 +15,27 @@ export const addPost = postData => dispatch => {
       })
     )
   );
+};
+
+// get Post
+export const getPost = () => dispatch => {
+  dispatch(setPostLoading());
+  axios.get('/api/posts').then(res =>
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data
+    }).catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    )
+  );
+};
+
+// Set Loading state
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
 };
