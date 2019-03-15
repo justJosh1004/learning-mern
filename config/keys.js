f7da682a7b62a1503213c502c392fe7eb1eb66bc
justJosh1004 <j.joson@live.com>
2019-03-15 10:09:53 -0400

    secured keys for prod

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    'mongodb://josh:josh@cluster0-shard-00-00-5yeui.mongodb.net:27017,cluster0-shard-00-01-5yeui.mongodb.net:27017,cluster0-shard-00-02-5yeui.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'propd') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
